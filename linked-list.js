05ef3db69a2b87e2eff007e855375fb858c349eb
SaToGo <stoledano97@gmail.com>
2023-02-08 01:31:21 +0100

    add linked-list.js

---
+++ linked-list.js
@@ -0,0 +1,16 @@
+
+class LinkedList{
+    constructor(initialNode = null){
+        this.headNode = initialNode;
+    }
+}
+
+class Node{
+    constructor(){
+        this.value = null;
+        this.nextNode = null;
+    }
+
+
+}
+
